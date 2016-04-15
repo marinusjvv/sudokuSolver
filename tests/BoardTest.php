0ab72404f84bcd4e87e50aa8e2f555de3d927310
Marinus Jansen van Vuuren <marinus.jvv1@gmail.com>
2016-04-15 15:52:27 +1000

    Added board

---
+++ tests/BoardTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace MarinusJvv\Tests;
+
+use MarinusJvv\Sudoku\Board\Board;
+
+class BoardTest extends \PHPUnit_Framework_TestCase
+{
+    public function testBoard()
+    {
+        $board = new Board();
+    }
+}
